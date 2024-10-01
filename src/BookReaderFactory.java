ab03ab40008c8d332f2d829bfebd6b6ba9f056b6
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-10-01 12:12:37 +0600

    modified the factory class

--- src/BookReaderFactory.java
+++ src/BookReaderFactory.java
@@ -1,15 +1,16 @@
 public class BookReaderFactory {
 
-    public static BookReader getBookReader(String fileType) {
-        switch (fileType.toLowerCase()) {
-            case "csv":
-                return new CSVBookReader();
-            case "json":
-                return new JSONBookReader();
-            case "xml":
-                return new XMLBookReader();
-            default:
-                throw new IllegalArgumentException("Unsupported file type: " + fileType);
+
+    public static BookReader getBookReader(String filePath) {
+        if (filePath.endsWith(".csv")) {
+            return new CSVBookReader();
+        } else if (filePath.endsWith(".xml")) {
+            return new XMLBookReader();
+        } else if (filePath.endsWith(".json")) {
+            return new JSONBookReader();
+        } else {
+            throw new IllegalArgumentException("Unsupported file format: " + filePath);
         }
     }
 }
+
